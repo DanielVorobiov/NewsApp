a18cebad2c10deaf5756904e33bcb214d3315874
Daniel Vorobiov <vorobiov.daniel@gmail.com>
2020-11-08 10:17:59 +0200

    Initial Commit

--- App.js
+++ App.js
@@ -1,13 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+import React , {useState, useEffect} from 'react';
+import { StyleSheet, Text, View, SafeAreaView, FlatList,ActivityIndicator, Linking, Image, TouchableOpacity} from 'react-native';
+
+
+const URL = 'http://newsapi.org/v2/top-headlines?' +
+'country=in&' +
+'apiKey=d3617fc0504c41d0a430bebf6ba1ca63';
+
+export default function App(){
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+  const [title,setTitle] = useState([]);
+
+  useEffect(() => { 
+    fetch(URL).then((response) => response.json())
+    .then((json) => {setData(json.articles); setTitle(json.title)})
+    .catch((error) => alert(error))
+    .finally(() => setLoading(false));
+  },[]);
+  
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{paddingTop:50,paddingLeft:10, backgroundColor: '#c9f5f5',}}>
+      {isLoading ? <ActivityIndicator/>:
+        <View> 
+        <Text>{title}</Text>
+        <FlatList 
+        data = {data}
+        keyExtractor={({id},index) => id}
+        renderItem={({item}) => {
+          return(
+          <View style = {{flexDirection: 'row',}}> 
+            <View style={{width: 50, height: 50, marginRight:10, }} >
+              <TouchableOpacity onPress = {() => Linking.openURL(item.url)}>
+                <Image  style={{width: 50, height: 50,}}  source={{uri: (item.urlToImage)}}></Image>
+              </TouchableOpacity>
+            </View>
+            <View style={{marginRight:10,width:340}}>
+              <Text key={0} style={{}}  >{item.title}</Text>
+              <Text key={1} style={{color:'blue'}} onPress = {() => Linking.openURL(item.url)}>URL:  {item.url} {"\n"}</Text>
+            </View>
+          </View>
+          );
+  
+        }}
+        />
+      </View>
+      }
+    </SafeAreaView>
   );
 }
 
@@ -18,4 +59,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
 });
+
+
